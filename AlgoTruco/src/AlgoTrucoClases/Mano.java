8e89426deea0e146f90eb346ba621183fbc7d089
MARIA FLORENCIA PRADO <mfprado@icloud.com>
2015-11-09 21:37:53 -0300

    Mano

--- AlgoTruco/src/AlgoTrucoClases/Mano.java
+++ AlgoTruco/src/AlgoTrucoClases/Mano.java
@@ -1,5 +1,17 @@
 package AlgoTrucoClases;
 
+import java.util.ArrayList;
+
 public class Mano {
+
+	private ArrayList<Carta> cartas;
+
+	public Mano(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
+	}
+
+	public int cantidadDeCartas() {
+		return (this.cartas.size());
+	}
 
 }
