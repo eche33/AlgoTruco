04fc7af77280cd796d71c7d11960e002bd856988
Ailu González <ailengonzalezp@yahoo.com.ar>
2015-11-10 11:14:44 -0300

    Update Carta.java

--- AlgoTruco/src/AlgoTrucoClases/Carta.java
+++ AlgoTruco/src/AlgoTrucoClases/Carta.java
@@ -2,16 +2,33 @@ package AlgoTrucoClases;
 
 public class Carta {
 
-	private String palo;
+	private Palos palo;
 	private int valor;
+	private int prioridad; //Falta la asignación y los test
 
-	public Carta(int valor, String palo) {
-		this.palo = palo;
+	public Carta(int valor, Palos paloActual) {
+		this.palo = paloActual;
 		this.valor = valor;
 	}
 
 	public Boolean EsPalo(String palo) {
-		return (this.palo == palo);
+		return ( (this.palo).name() == palo);
+	}
+	
+	public String obtenerPalo() {
+		return ( (this.palo).name() );
+	}
+	
+	public int obtenerValor() {
+		return (this.valor);
+	}
+	
+	public int obtenerPrioridad(){
+		return (this.prioridad);
+	}
+	
+	public void asignarPrioridad(int importancia){
+		this.prioridad = importancia;
 	}
 
 }
