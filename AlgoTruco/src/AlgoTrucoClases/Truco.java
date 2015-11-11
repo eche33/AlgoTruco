87793ce1e43edc359983d926a5c89e2e34d37938
Cris <cristian.rana@hotmail.com>
2015-11-11 20:17:03 -0300

    Update Truco.java

--- AlgoTruco/src/AlgoTrucoClases/Truco.java
+++ AlgoTruco/src/AlgoTrucoClases/Truco.java
@@ -2,4 +2,12 @@ package AlgoTrucoClases;
 
 public class Truco {
 
+    private Partida partida;
+    
+    public Truco(){}
+    
+    public void crearPartida(){
+        //HAY QUE DEFINIR PARTIDA SI ES SINGLETON, POR SU INVOCACIÓN.
+    }
+
 }
