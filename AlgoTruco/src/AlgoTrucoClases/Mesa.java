26bc6c41cd893bb803cd4cad46c9a505a766e194
Rodrigo <rorroeche@gmail.com>
2015-11-20 17:55:34 -0300

    Creacion Mesa

---
+++ AlgoTruco/src/AlgoTrucoClases/Mesa.java
@@ -0,0 +1,13 @@
+package AlgoTrucoClases;
+
+import java.util.ArrayList;
+
+public class Mesa {
+
+	private ArrayList<Carta> cartasTiradas;
+
+	public Mesa() {
+		this.cartasTiradas = new ArrayList<Carta>();
+	}
+
+}
